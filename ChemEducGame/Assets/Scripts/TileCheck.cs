99951d407e20b4876d8911c51e8102ad5db6d776
Elijah Jeremiah L. Barba <bit.elijah.barba@gmail.com>
2022-08-29 00:50:16 +0800

    Force uppercase in InputField

--- ChemEducGame/Assets/Scripts/TileCheck.cs
+++ ChemEducGame/Assets/Scripts/TileCheck.cs
@@ -51,23 +51,33 @@ public class TileCheck : MonoBehaviour
         }
    }
 
+
    public void GetFirstEmptyTile()
    {
         //if word is cleared after wrong answer, force active input field on first tile
         if (answer[0].GetComponent<TMP_InputField>().text == "")
         {
+            //focus player to type in this tile
             answer[0].GetComponent<TMP_InputField>().ActivateInputField();
+            //force uppercase
+            answer[0].GetComponent<TMP_InputField>().onValidateInput += delegate (string s, int i, char c) { return char.ToUpper(c); };
         }
         else
         {
+            
             for (int i = 0; i < answer.Length; i++)
             {
                 //activate input field on first empty tile
                 if (answer[i].GetComponent<TMP_InputField>().text == "")
                 {
+                    //focus player to type in this tile
                     answer[i].GetComponent<TMP_InputField>().ActivateInputField();
+                    //force uppercase
+                    answer[i].GetComponent<TMP_InputField>().onValidateInput += delegate (string s, int i, char c) { return char.ToUpper(c); };
                 }
             }
         }
    }
+
+   
 }
