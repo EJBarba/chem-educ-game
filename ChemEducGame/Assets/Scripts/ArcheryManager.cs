e01e2795d6ea6a9391e8e4d17e56841c91ca7920
Elijah Jeremiah L. Barba <bit.elijah.barba@gmail.com>
2022-09-13 04:30:00 +0800

    Fix Level 2 bugs with next level button

--- ChemEducGame/Assets/Scripts/ArcheryManager.cs
+++ ChemEducGame/Assets/Scripts/ArcheryManager.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ArcheryManager : MonoBehaviour
 {
     
     [SerializeField] GameObject resultModal;
     [SerializeField] TMP_Text resultText;
+    [SerializeField] GameObject nextLevelButton;
     public bool birdCollided = false;
     public bool isTargetCollided = false;
     private bool isHitOnce = true;
@@ -19,7 +21,12 @@ public class ArcheryManager : MonoBehaviour
 
             if (isTargetCollided == true)
             {
-                resultText.text = "YOU WIN"; 
+                resultText.text = "YOU WIN";
+                if(SceneManager.GetActiveScene().name != "Archery1")
+                {
+                    nextLevelButton.SetActive(true); 
+                }
+                
             }
             else
             {
