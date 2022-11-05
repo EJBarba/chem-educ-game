0e27cdb7b4a282ed95e12d54811e31a84f201aac
Elijah Jeremiah L. Barba <bit.elijah.barba@gmail.com>
2022-11-05 12:01:07 +0800

    Add more laser logic

--- ChemEducGame/Assets/Scripts/Laser.cs
+++ ChemEducGame/Assets/Scripts/Laser.cs
@@ -5,15 +5,39 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     public Transform firePoint;
+    public Transform ironSightPoint;
     public LineRenderer lineRenderer;
     public ArcheryManager archeryManager;
-    // Update is called once per frame
+    public LineRenderer lineRendererIronSight;
+    private int laserHit = 0;
+    
+    private void Start() {
+        lineRendererIronSight.SetPosition(0, ironSightPoint.position);
+    }
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        RaycastHit2D hitInfoIronSight = Physics2D.Raycast(ironSightPoint.position, ironSightPoint.right);
+
+        if (Input.GetKeyUp("space"))
+        {
+            lineRenderer.enabled = false;
+        }
+        if (Input.GetKey("space"))
         {
             StartCoroutine(FireLaser());
         }
+
+        if (hitInfoIronSight)
+        {
+          lineRendererIronSight.SetPosition(1, hitInfoIronSight.point);  
+        }
+
+        if(!hitInfoIronSight)
+        {
+            lineRendererIronSight.SetPosition(1, ironSightPoint.position + firePoint.right * 100);
+        }
+        
+        
     }
     public IEnumerator FireLaser()
     {
@@ -24,14 +48,17 @@ public class Laser : MonoBehaviour
             lineRenderer.SetPosition(0, firePoint.position);
 			lineRenderer.SetPosition(1, hitInfo.point);
 
-            archeryManager.isLaserFired = true;
+            //archeryManager.isLaserFired = true;
             if(hitInfo.transform.tag == "Target")
             {
-                archeryManager.isTargetHit = true;
+                //archeryManager.isTargetHit = true;
+                laserHit += 1;
+                Debug.Log(laserHit);
+                
             }
             else if (hitInfo.transform.tag == "Dummy")
             {
-                archeryManager.isTargetHit = false;
+                //archeryManager.isTargetHit = false;
             }
         }
         else
@@ -45,6 +72,5 @@ public class Laser : MonoBehaviour
         
         lineRenderer.enabled = true;
 		yield return new WaitForSeconds(0.02f);
-		lineRenderer.enabled = false;
     }
 }
