798c7da1424c9db6f806fd024c1b9132475cedb2
Elijah Jeremiah L. Barba <bit.elijah.barba@gmail.com>
2022-11-07 20:11:02 +0800

    Add laser_level2_1 scene

--- ChemEducGame/Assets/Scripts/SceneHandler.cs
+++ ChemEducGame/Assets/Scripts/SceneHandler.cs
@@ -89,4 +89,8 @@ public class SceneHandler : MonoBehaviour
     {
         SceneManager.LoadScene("Laser_Level1_Results");
     }
+    public void LaserLevel2_1()
+    {
+        SceneManager.LoadScene("Laser_Level2_1");
+    }
 }
