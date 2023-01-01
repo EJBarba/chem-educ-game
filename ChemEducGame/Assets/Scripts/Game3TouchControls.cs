da6a78635b2cd2324048caad8254378463128a75
Elijah Jeremiah L. Barba <bit.elijah.barba@gmail.com>
2023-01-01 16:17:56 +0800

    Update optimization settings and touch control bug fix

--- ChemEducGame/Assets/Scripts/Game3TouchControls.cs
+++ ChemEducGame/Assets/Scripts/Game3TouchControls.cs
@@ -7,11 +7,16 @@ public class Game3TouchControls : MonoBehaviour
     [SerializeField] GameObject player;
     public void MovePlayerUp()
     {
-        player.transform.position += new Vector3(0f, 2f, 0f);   
+        if (player.transform.position.y < 0.75)
+        {
+            player.transform.position += new Vector3(0f, 2f, 0f); 
+        }
     }
     public void MovePlayerDown()
     {
-        player.transform.position += new Vector3(0f,-2f, 0f);   
+        if (player.transform.position.y > -3.25)
+        {
+           player.transform.position += new Vector3(0f,-2f, 0f);  
+        }
     }
-    
 }
